69990bfb434535348f6c5363eb6a81f218ef6603
Jonathan Riego <Jonathan.riego@ufl.edu>
2020-11-08 18:31:30 -0500

    Fixed merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,9 @@ import "./App.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-function App() {
-=======
+
 
 const App = () => {
->>>>>>> Stashed changes
-=======
-const App = () => {
->>>>>>> 6a543a9d858b85d459565b99087a2463f3c909b1
   return (
     // switch determines which component to return based on the path
     <Switch>
