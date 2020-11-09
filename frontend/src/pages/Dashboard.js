7d3401bc7817a3a0372cc40dfc0b26d9196cbef3
alexistodd <alexis.todd@ufl.edu>
2020-11-08 23:01:21 -0500

    added graphs to dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import '../css/Dashboard.css'
+import ProductChart from '../components/ProductSales';
+import VendorChart from '../components/VendorSales';
+
 
 function Dashboard(){
     return(
         <div className='Dashboard'>
             <header className='Dashboard-header'>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
                 <div className='Dashboard-title'>
                     COMPANY NAME HERE
                 </div>
             </header>
             
             <div className='Dashboard-module1'>
-                module 1
+                SALES BY PRODUCT
+                <ProductChart/>
             </div>
 
+
+
             <div className='Dashboard-module2'>
-                module 2
+                SALES BY VENDOR
+                <VendorChart/>
             </div>
 
             <div className='Dashboard-module3'>
-                module 3
+               
             </div>
 
             <div className='Dashboard-module4'>
-                module 4
+               
             </div>
         </div>
     )
