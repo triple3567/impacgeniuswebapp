88c37725f4b513e11b8f8fbaf2105c373c95ae49
Mahan Mahtabfar <mmahtabfar1@gmail.com>
2020-12-08 18:38:41 -0500

    fixed signout button path

--- frontend/src/components/SignOutButton.js
+++ frontend/src/components/SignOutButton.js
@@ -1,23 +1,22 @@
-import React from "react"
-import auth from './LoginBox.js'
+import React from "react";
+import auth from "./LoginBox.js";
 import firebase from "firebase/app";
 import "firebase/auth";
-import { Button } from 'antd';
-
+import { Button } from "antd";
 
 const SignOutButton = (props) => {
+  const onSignOut = (props) => {
+    firebase.auth().signOut();
+    window.location.href = "/";
+  };
 
-    const onSignOut = (props) => {
-        firebase.auth().signOut();
-        window.location.href = "http://localhost:3000"
-
-    }
-
-    return (
-        <div className='SignOutButton'> 
-            <Button type="primary" onClick = {onSignOut}> Sign Out </Button>
-        </div>
-    )
-
-}
+  return (
+    <div className="SignOutButton">
+      <Button type="primary" onClick={onSignOut}>
+        {" "}
+        Sign Out{" "}
+      </Button>
+    </div>
+  );
+};
 export default SignOutButton;
